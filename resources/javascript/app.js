8cab8da747f70467bb4c34207d997f847286c568
monsterreh2b <brant.han@gmail.com>
2017-04-18 15:06:20 -0700

    Counters work

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,6 +1,6 @@
 
 //set number to 1 min 30s
-   var number = 90;
+   var number = 20;
 
    var intervalId;
 
@@ -13,30 +13,29 @@
 //start game
 $("#start").on("click",run);
 
-$("#submit").click(function() {
- $("#correct").html("<h3> Correct Answers: </h3>" + correctCount);
- 	$("#incorrect").html("<h3> Inorrect Answers: </h3>" + inCorrectCount);
- 	$("#unanswered").html("<h3> Unanswered: </h3>" + unAnsweredCount);
- 	document.getElementById("questionsTwo").innerHTML = document.getElementById("endGame").innerHTML;
-
-  });
-
+$("#submit").on("click",run);
+ 
+  
 function handleClick(){         
          
 for(var i = 1; i <= 6; i++) {
-  var radios = document.getElementsByName('question'+i);
-  for(var j = 0; j < radios.length; j++) {
-    var radio = radios[j];
-    if(radio.id== "correct" && radio.checked) {
-      correctCount++;
-    }else if (radio.id== "incorrect" && radio.checked){
-    	inCorrectCount++;
-    }else{
-    	unAnsweredCount++;
+    var radios = document.getElementsByName('question'+i);
+    for(var j = 0; j < radios.length; j++) {
+      var radio = radios[j];
+      if(radio.id== "correct" && radio.checked) {
+        correctCount++;
+      }else if (radio.id== "inCorrect" && radio.checked){
+      	inCorrectCount++;
+      }
     }
-  }
- }                   
-    alert("Correct Responses: " + correctCount);
+if (radios.checked==false){
+        unAnsweredCount++;
+      }  
+   }
+
+      alert("Correct Responses: " + correctCount);
+       alert("InCorrect Responses: " + inCorrectCount);
+    
   }
 
 function decrement(){
@@ -51,6 +50,7 @@ function decrement(){
 		alert("You've run out of time");
 		handleClick();
 		end();
+    changeTwo();
 	}
 }
 
@@ -58,10 +58,24 @@ function change() {
     document.getElementById("questions").innerHTML = document.getElementById("questionsTwo").innerHTML;
    }
 
+function changeTwo(){
+  document.getElementById("endGameCorrect").innerHTML= "<h3> Correct Answers: </h3>" + correctCount;
+  document.getElementById("endGameIncorrect").innerHTML="<h3> Incorrect Answers: </h3>" + inCorrectCount;
+  document.getElementById("endGameUnanswered").innerHTML = "<h3> Unanswered: </h3>" + unAnsweredCount;
+  document.getElementById("questions").innerHTML = document.getElementById("endGame").innerHTML;
+ 
+
+  
+}
+
  function run(){
 	intervalId = setInterval(decrement, 1000);
 	change();
 }
+
+
+
+
 function stop(){
 
 	clearInterval(intervalId);
@@ -74,7 +88,7 @@ function check(){
 }
 
 function end(){
- 	number ==90;
+ 	number ==20;
  	
  }
 
